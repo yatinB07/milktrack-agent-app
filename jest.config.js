35863412b399608d06d3c986b097c47c08ffce60
YatinB07 <ybhanderi@gmail.com>
2026-07-23 13:15:19 +0530

    fix: provide deterministic Jest API origin

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,1 @@
-module.exports = { preset: 'jest-expo', testMatch: ['**/__tests__/**/*.test.ts?(x)'], moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' } };
+module.exports = { preset: 'jest-expo', setupFiles: ['<rootDir>/test/jest.setup.js'], testMatch: ['**/__tests__/**/*.test.ts?(x)'], moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' } };
---
+++ test/jest.setup.js
@@ -0,0 +1,1 @@
+process.env.EXPO_PUBLIC_API_BASE_URL ??= 'http://localhost:3000';
